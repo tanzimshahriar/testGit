2e99947f3193c3d4df505c6c200dc5639d8c4aa1
Tanzim Shahriar <tanzimshahriar@Tanzims-Mac.local>
2019-09-21 04:02:04 -0700

    Initial Commit

---
+++ testGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by Tanzim Shahriar on 21/9/19.
+//  Copyright © 2019 Tanzim Shahriar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ testGitTests/testGitTests.swift
@@ -0,0 +1,34 @@
+//
+//  testGitTests.swift
+//  testGitTests
+//
+//  Created by Tanzim Shahriar on 21/9/19.
+//  Copyright © 2019 Tanzim Shahriar. All rights reserved.
+//
+
+import XCTest
+@testable import testGit
+
+class testGitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
